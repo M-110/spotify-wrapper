86ca2b5d2f460b107ca59c82f02c10b1fd2ad48a
DESKTOP-75401JP\mclea <mcleancolinm@gmail.com>
2021-03-23 16:16:26 -0500

    Start a new framwork for converting a yaml file into a large set of methods for all the possible API calls

---
+++ api_methods/method_declaring_helper.py
@@ -0,0 +1,75 @@
+﻿import yaml
+import textwrap
+
+header_template = '''
+{requires_decorator}
+def {method_name}({params}) -> {return_type}:
+    """
+{method_doc}
+    """
+    
+    
+    response = self._{http_method}(url)
+'''
+
+prop_template = '''
+    @property
+    def {attr_name}(self) -> {attr_return}:
+        """
+{attr_doc}
+        """
+        return self._json_dict['{attr_name}']
+'''
+
+
+def class_wrap(class_text: str):
+    return '\n'.join(
+        textwrap.fill(line,
+                      width=78,
+                      initial_indent=' ' * 4,
+                      subsequent_indent=' ' * (4 + len(line) - len(line.strip())))
+        for line in class_text.splitlines())
+
+
+def attr_wrap(attr_text: str):
+    return '\n'.join(
+        textwrap.fill(line,
+                      width=78,
+                      initial_indent=' ' * 8,
+                      subsequent_indent=' ' * (8 + len(line) - len(line.strip())))
+        for line in attr_text.splitlines())
+
+
+def yaml_to_class(filename):
+    with open(filename, 'r', encoding='utf-8') as yaml_file:
+        yaml_dict = yaml.load(yaml_file, Loader=yaml.Loader)
+
+    output = ''
+
+    for class_ in yaml_dict:
+        header = header_template.format(class_name=class_['name'],
+                                        class_doc=class_wrap(class_['doc']))
+        output += header
+        for attr in class_['attrs']:
+            prop = prop_template.format(attr_name=attr['name'],
+                                        attr_doc=attr_wrap(attr['doc']),
+                                        attr_return=attr['return'])
+            output += prop
+        output += '\n'
+
+    return output
+
+
+def yaml_to_methods(filename):
+    with open(filename, 'r', encoding='utf-8') as yaml_file:
+        yaml_dict = yaml.load(yaml_file, Loader=yaml.Loader)
+
+    output = ''
+
+    for api in yaml_dict:
+        for method in api['methods']:
+            print(method)
+    return output
+
+class_output = yaml_to_methods('methods.yaml')
+print(class_output)
---
+++ api_methods/validators.py
@@ -0,0 +1,38 @@
+﻿def validate_type(value,  param_name: str, expected_type):
+    """Valid value is the same as expected_type."""
+    if not isinstance(value, expected_type):
+        raise TypeError(f'{param_name!r} must be of type {expected_type}')
+
+
+def validate_range(num: int, param_name: str, min_: int, max_: int,):
+    """Validate the num is between the inclusive range of min_ and max_."""
+    if not ((num >= min_) and (num <= max_)):
+        raise ValueError(f'{param_name!r} must be in the range of  {min_} to {max_}')
+
+
+def validate_country(country_code: str, param_name: str):
+    """Validate the country code is a valid 2 letter country code."""
+    valid_codes = ['AF', 'AL', 'DZ', 'AS', 'AD', 'AO', 'AI', 'AQ', 'AG', 'AR', 'AM', 'AW', 'AU',
+                   'AT', 'AZ', 'BS', 'BH', 'BD', 'BB', 'BY', 'BE', 'BZ', 'BJ', 'BM', 'BT', 'BO',
+                   'BQ', 'BA', 'BW', 'BV', 'BR', 'IO', 'BN', 'BG', 'BF', 'BI', 'CV', 'KH', 'CM',
+                   'CA', 'KY', 'CF', 'TD', 'CL', 'CN', 'CX', 'CC', 'CO', 'KM', 'CD', 'CG', 'CK',
+                   'CR', 'HR', 'CU', 'CW', 'CY', 'CZ', 'CI', 'DK', 'DJ', 'DM', 'DO', 'EC', 'EG',
+                   'SV', 'GQ', 'ER', 'EE', 'SZ', 'ET', 'FK', 'FO', 'FJ', 'FI', 'FR', 'GF', 'PF',
+                   'TF', 'GA', 'GM', 'GE', 'DE', 'GH', 'GI', 'GR', 'GL', 'GD', 'GP', 'GU', 'GT',
+                   'GG', 'GN', 'GW', 'GY', 'HT', 'HM', 'VA', 'HN', 'HK', 'HU', 'IS', 'IN', 'ID',
+                   'IR', 'IQ', 'IE', 'IM', 'IL', 'IT', 'JM', 'JP', 'JE', 'JO', 'KZ', 'KE', 'KI',
+                   'KP', 'KR', 'KW', 'KG', 'LA', 'LV', 'LB', 'LS', 'LR', 'LY', 'LI', 'LT', 'LU',
+                   'MO', 'MG', 'MW', 'MY', 'MV', 'ML', 'MT', 'MH', 'MQ', 'MR', 'MU', 'YT', 'MX',
+                   'FM', 'MD', 'MC', 'MN', 'ME', 'MS', 'MA', 'MZ', 'MM', 'NA', 'NR', 'NP', 'NL',
+                   'NC', 'NZ', 'NI', 'NE', 'NG', 'NU', 'NF', 'MP', 'NO', 'OM', 'PK', 'PW', 'PS',
+                   'PA', 'PG', 'PY', 'PE', 'PH', 'PN', 'PL', 'PT', 'PR', 'QA', 'MK', 'RO', 'RU',
+                   'RW', 'RE', 'BL', 'SH', 'KN', 'LC', 'MF', 'PM', 'VC', 'WS', 'SM', 'ST', 'SA',
+                   'SN', 'RS', 'SC', 'SL', 'SG', 'SX', 'SK', 'SI', 'SB', 'SO', 'ZA', 'GS', 'SS',
+                   'ES', 'LK', 'SD', 'SR', 'SJ', 'SE', 'CH', 'SY', 'TW', 'TJ', 'TZ', 'TH', 'TL',
+                   'TG', 'TK', 'TO', 'TT', 'TN', 'TR', 'TM', 'TC', 'TV', 'UG', 'UA', 'AE', 'GB',
+                   'UM', 'US', 'UY', 'UZ', 'VU', 'VE', 'VN', 'VG', 'VI', 'WF', 'EH', 'YE', 'ZM',
+                   'ZW', 'AX']
+
+    if country_code.upper() not in valid_codes:
+        raise ValueError(f'{country_code!r} is not a valid 2 letter country code. '
+                         'See: https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes')
